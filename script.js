3e95982b1946d486975a7197d5267bf9806d88bb
TyomaKirakos <98010508+TyomaKirakos@users.noreply.github.com>
2022-01-20 11:20:01 +0300

    Optimized the code

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const weeksBlock = document.querySelector('.weeks-block');
 let currentDay = new Date().getDay();
-console.log(currentDay);
-console.log(week[currentDay - 1]);
-
 
 week.forEach((day) => {
     let weekDay = document.createElement('p');
     if (day == week[currentDay - 1]){
         weekDay.className = "currDay";
-        weekDay.textContent = day;
     } else if (day == "Saturday" || day == "Sunday"){
         weekDay.className = "weekend";
-        weekDay.textContent = day;
     } else {
         weekDay.className = "justDay";
-        weekDay.textContent = day;
     }
+    weekDay.textContent = day;
     weeksBlock.append(weekDay);
 })
